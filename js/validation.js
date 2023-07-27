53c993fb276873d8be4e032e0daf21bc611507c6
jeslinjayanvarghese <jeslinjaisan@gmail.com>
2023-07-27 17:23:03 +0530

    Jephy portfolio

---
+++ js/validation.js
@@ -0,0 +1,106 @@
+$("#submit-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycbzG3MId9VCrfjTfSvsvI1ClumLB7qLnGXCtos4u9kfBJY8Ejxs_di6wXLNv1X6d7La0/exec",
+        data:$("#submit-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            //window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error")
+
+        }
+    })
+})
+
+function validateName(){
+
+  var nameField = document.getElementById('contact-name').value;
+
+  if(nameField.length == 0){
+
+      nameError.innerHTML="Name is required";
+
+      return false;
+
+  }
+
+  if(!nameField.match(/^[A-Za-z0-9]*\s{1}[A-Za-z]+$/) ){
+    
+      nameError.innerHTML="Write fullname";
+
+      return false;
+  }else{
+      nameError.innerHTML='<i class="fa-solid fa-check"></i>';
+      return true;
+  
+  }
+  
+
+}
+function validateEmail(){
+
+  var emailField = document.getElementById('contact-email').value;
+  
+  if(emailField.length==0)
+  {
+      emailError.innerHTML="Email is required";
+      return false;
+  }
+
+ if(!emailField.match(/^[A-Za-z\._\-0-9][@][A-Za-z][\.][a-z]{2,4}$/)){
+  emailError.innerHTML="invalid email";
+  return false;
+}else{
+  emailError.innerHTML='<i class="fa-solid fa-check"></i>';
+  return true;
+}
+}
+function validateSubject(){
+
+  var subjField = document.getElementById('contact-subject').value;
+
+  var required =30;
+
+  var left = required- subjField.length;
+
+  if(left > 0){
+      subjError.innerHTML = left + 'more characters required';
+  return false;
+  }else{
+      subjError.innerHTML ='<i class="fa-solid fa-check"></i>';
+      return true;
+  }
+  
+
+}
+function validateMessage(){
+
+  var msgField = document.getElementById('contact-message').value;
+
+  var required =50;
+
+  var left = required- msgField.length;
+
+  if(left > 0){
+      msgError.innerHTML = left + 'more characters required';
+  return false;
+  }else{
+      msgError.innerHTML ='<i class="fa-solid fa-check"></i>';
+      return true;
+  }
+  
+
+}
+function validateForm()
+{
+if(!validateName() || !validatePhone() || !validateEmail()||!validateMessage()){
+
+    submitError.innerHTML="Please fix these error to sumbit ";
+    return false;    
+}
+
+}
